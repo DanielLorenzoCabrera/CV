c141052a9ac3ff6f1d60c7ed416ced3d1671505a
Daniel Lorenzo <juan.lorenzo@singularfactory.net>
2022-06-09 22:42:38 +0100

    Working with modal header icons

--- src/components/Modal/ModalHeader.js
+++ src/components/Modal/ModalHeader.js
@@ -4,11 +4,21 @@ import { closeAll } from "../../features/modalSlice";
 const ModalHeader = () => {
   const dispatch = useDispatch();
   return (
-      <div className="modal-header">
-        <article className="modal-icon close" onClick={() => dispatch(closeAll())}></article>
-        <article className="modal-icon minimize" onClick={() => dispatch(closeAll())}></article>
-        <article className="modal-icon expand"></article>
-      </div>
+    <div className="modal-header">
+      <article
+        className="modal-icon close"
+        onClick={() => dispatch(closeAll())}
+      >
+        <span>x</span>
+      </article>
+      <article
+        className="modal-icon minimize"
+        onClick={() => dispatch(closeAll())}
+      ><span>&#8213;</span></article>
+      <article className="modal-icon expand">
+        <span>&#60;&#62;</span>
+      </article>
+    </div>
   );
 };
 
