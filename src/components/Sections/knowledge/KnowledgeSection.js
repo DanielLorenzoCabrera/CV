11869ed1b2cf2eeea8f2335aed87c666bd4687a9
Daniel Lorenzo <juan.lorenzo@singularfactory.net>
2022-07-26 20:17:21 +0100

    Second version of backend section

--- src/components/Sections/knowledge/KnowledgeSection.js
+++ src/components/Sections/knowledge/KnowledgeSection.js
@@ -7,11 +7,7 @@ const KnowledgeSection = ({ skills, title, image }) => {
       <div>
         <img src={image}></img>
         {skills.map((skill, index) => {
-          return skill.name === "API" ? (
-            <div className="skill">
-              <p>{skill.name}</p>
-            </div>
-          ) : (
+          return (
             <Skill key={index} name={skill.name} icon={skill.icon}></Skill>
           );
         })}
